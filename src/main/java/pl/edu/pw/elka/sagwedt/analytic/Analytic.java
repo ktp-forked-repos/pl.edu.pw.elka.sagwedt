8b679f807968d1581f52b0729e482d31c5e76571
BZWGolem <BZWGolem@192.168.1.113>
2017-11-15 14:37:23 +0100

    Simple methods that get m2 info from string

--- src/main/java/pl/edu/pw/elka/sagwedt/analytic/Analytic.java
+++ src/main/java/pl/edu/pw/elka/sagwedt/analytic/Analytic.java
@@ -15,11 +15,13 @@ public class Analytic {
 
 	public static void main(String[] args) {
 
-		PolishStemmer stemmer = new PolishStemmer();
-
-		String in = "Mieszkanie 20mkw wynajmę za 500zł miesięcznie plus media. Starość nie radość, śmierć nie wesele. Lubie placki z jagodami. Kocham Piotrusia.";
-
+		String in = "klimatyczne mieszkanie na Bielanach 2 900 zł Dodaj takie ogłoszenie! Data dodania 03/11/2017 Lokalizacja Bielany, Warszawa Do wynajęcia przez Właściciel Dostępny 01/12/2017 Rodzaj nieruchomości Mieszkanie Liczba pokoi 3 pokoje Liczba łazienek 2 łazienki Wielkość (m2) 75 Parking Kryty Palący Tak Przyjazne zwierzakom Nie Mieszkanie usytuowane jest na pierwszym piętrze w 4-piętrowym, jednoklatkowym budynku, w środku chronionego osiedla. W zielonej i spokojnej okolicy, przy ul. Kochanowskiego. Składa się z 3 pokoi (salon połączony z kuchnią, 2 sypialnie; przy salonie i 1 sypialni są balkony), widnej łazienki z wanną, wc, garderoby. Jest w pełni umeblowane (TV, meble, szafy wnękowe, kuchenka z piekarnikiem, lodówka z zamrażarką, akcesoria kuchenne, pralka). Miejsce jest bardzo dobrze skomunikowane z centrum miasta- autobusy wzdłuż ul. Kochanowskiego, tramwaje przy ul. Broniewskiego, blisko stacji Metro Stare Bielany. Miejsce parkingowe w garażu podziemnym. Kaucja w wysokości 1-miesięcznego czynszu.";
+		String in2 = "Obserwuj Galeria Wynajmę mieszkanie Warszawa, Mazowieckie, Mokotów Dodane o 18:09, 14 listopada 2017, ID ogłoszenia: 386120665 Wyróżnij to ogłoszenie Odśwież to ogłoszenie Oferta od Osoby prywatnej Poziom 2 Umeblowane Tak Rodzaj zabudowy Blok Powierzchnia 36 m² Liczba pokoi Kawalerka Czynsz (dodatkowo) 1 zł Mieszkanie 1-pokojowe na 2-gim piętrze, w bloku 4 piętrowym z 2003 roku, urządzone:lodówka,pralka,zmywarka.W budynku 2 windy i ochrona 24h.Do mieszkania przynależy podziemny garaż.Okolica cicha,pełna zieleni i terenów rekreacyjnych.Blisko sklepy i przystanek. Następne ogłoszenie Wróć Wyświetleń:558";
 		printInfo(in);
+		printInfo(in2);
+		
+		System.out.println("Metry1: " + getM2(in));
+		System.out.println("Metry2: " + getM2(in2));
 
 	}
 	
@@ -54,6 +56,46 @@ public class Analytic {
 
 	private static List<Word> getWordText(String original_text) {
 		return getWordText(getStringWords(original_text));
+	}
+	
+	private static List<Word> getWordsCloseToMetersInfo(List<Word> input_list) {
+		List<Word> ret = new ArrayList<>();
+		Word before = null;
+		boolean gettingLast = false;
+		for( Word w : input_list) {
+			if(gettingLast) {
+				ret.add(w);
+				return ret;
+			}
+			if((w.original.contains("mkw") || w.original.contains("m2") || w.original.contains("m²"))&& !gettingLast ) {
+				ret.add(before);
+				ret.add(w);
+				gettingLast = true;
+			}
+			before = w;
+		}
+		return null;
+	}
+	
+	private static boolean isInteger(String str) {
+	    int size = str.length();
+
+	    for (int i = 0; i < size; i++) {
+	        if (!Character.isDigit(str.charAt(i))) {
+	            return false;
+	        }
+	    }
+
+	    return size > 0;
+	}
+	
+	private static Integer getM2(String full_input) {
+		List<Word> words = getWordsCloseToMetersInfo(getWordText(full_input));
+		for(Word w : words) {
+			if( isInteger(w.original))
+				return Integer.parseInt(w.original);
+		}
+		return null;
 	}
 
 	/*
